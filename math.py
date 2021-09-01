a07b65d4ebdc618cec26bb46b81ba2aa4d1412e4
muskaan2199 <76113902+muskaan2199@users.noreply.github.com>
2021-09-01 16:24:45 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,22 @@
+#Add implementation
+def add(x,y):
+  return(x+y)
+
+#Subtract implementation  
+def subtract(x,y):
+  return(x-y)
+
+#Multiplication implementation  
+def multiply(x,y):
+  return(x*y)
+
+#Division implementation  
+def divide(x,y):
+  if y==0:
+    return DIVIDEBYZEROERROR
+  else:
+    return(x/y) 
+
+def square(x):
+  return(x*x)
+
